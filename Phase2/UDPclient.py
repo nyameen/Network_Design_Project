67e10ca580090b9539b89f3962e8867b72ee4f26
jswz72 <jacobdasword@gmail.com>
2019-02-04 17:17:23 -0500

    modify whitespace in comments

--- Phase2/UDPclient.py
+++ Phase2/UDPclient.py
@@ -2,46 +2,46 @@ import socket
 import time
 import select
 
-UDP_IP = "127.0.0.1"		# server IP
-UDP_PORT = 12001			# server Port
+UDP_IP = "127.0.0.1"    # server IP
+UDP_PORT = 12001    # server Port
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		# open a UDP socket
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# set options for resuse addr and port
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	# open a UDP socket
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # set options for resuse addr and port
 
 # change file name in file.jpg
-sock.sendto('file.jpg'.encode('utf-8'), (UDP_IP, UDP_PORT))	# send file name to server
+sock.sendto('file.jpg'.encode('utf-8'), (UDP_IP, UDP_PORT)) # send file name to server
 print ("Sending ")
 
 # change file name in file.jpg
-f = open("file.jpg", "rb")						# open that file for reading binary
-data = f.read(1024)								# read 1024 bytes
+f = open("file.jpg", "rb")  # open that file for reading binary
+data = f.read(1024) # read 1024 bytes
 
 # while there is still data to send
 while(data):
     if(sock.sendto(data, (UDP_IP, UDP_PORT))):	# send data to server
-        data = f.read(1024)						# read another 1024
-        time.sleep(0.02)						# small wait
+        data = f.read(1024) # read another 1024
+        time.sleep(0.02)    # small wait
 
 
-f.close()										# close file
+f.close()
 
 
-data, addr = sock.recvfrom(1024)				# receive file name from server
+data, addr = sock.recvfrom(1024)    # receive file name from server
 if(data):
-    print("File Name:", data)					# print it
-    file = data.strip()							# strip the name
+    print("File Name:", data)   # print it
+    file = data.strip() # strip the name
 
-f = open(file, "wb")							# open that file for writing binary
+f = open(file, "wb") # open that file for writing binary
 
 while(1):
     ready = select.select([sock], [], [], 3)	# wait until sock is ready for I/O
     if(ready[0]):
-        data, addr = sock.recvfrom(1024)		# read 1024 from server
-        f.write(data)							# write it to a file
+        data, addr = sock.recvfrom(1024)    # read 1024 from server
+        f.write(data)	# write it to a file
     else:
-        print("%s Finsih!" % file)				# finished reading file
+        print("%s Finsih!" % file)  # finished reading file
         break
 
-sock.close()		# close socket
-f.close()			# close port
+sock.close()    # close socket
+f.close()   # close port
--- Phase2/UDPserver.py
+++ Phase2/UDPserver.py
@@ -9,49 +9,49 @@ def sendImg(socket, client):
     socket.sendto('file.png'.encode('utf-8'), client)
     print ("Sending...")
 
-	# open that image for reading binary
+    # open that image for reading binary
     f = open("file.png", "rb")
 	
-	# read 1024 bytes
+    # read 1024 bytes
     data = f.read(1024)
 
-	# while theres data keep sending
+    # while theres data keep sending
     while(data):
-		# send 1024 bytes to client
+        # send 1024 bytes to client
         if(socket.sendto(data, client)):
-			# read another 1024
+            # read another 1024
             data = f.read(1024)
-			# slight delay
+            # slight delay
             time.sleep(0.02)
 
 
 
 UDP_IP = "127.0.0.1"	# Server IP
-IN_PORT = 12001			# port
-timeout = 3				# timeout
+IN_PORT = 12001	    # port
+timeout = 3	# timeout
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		# open a UDP socket
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# set the socket options to reuse address and port
-sock.bind((UDP_IP, IN_PORT))								# bind to address and port
+sock.bind((UDP_IP, IN_PORT))    # bind to address and port
 
 # forever
 while True:
-    data, addr = sock.recvfrom(1024)		# receive from client
+    data, addr = sock.recvfrom(1024)    # receive from client
     if data:
-        print ("File name:", data)			# print file received
-        file_name = data.strip()			# strip the header
+        print ("File name:", data)  # print file received
+        file_name = data.strip()    # strip the header
 
-    f = open(file_name, 'wb')				# open that file for writing binary
+    f = open(file_name, 'wb')   # open that file for writing binary
 
     while True:
         ready = select.select([sock], [], [], timeout) # wait until sock is ready for I/O
         if ready[0]:
             data, addr = sock.recvfrom(1024)	# receive 1024 from client
-            f.write(data)						# write it to a file
+            f.write(data)   # write it to a file
         else:
-            print ("%s Finish!" % file_name)	# finished writing 
-            f.close()							# close file
+            print ("%s Finish!" % file_name)    # finished writing 
+            f.close()	# close file
             break
 
     sendImg(sock, addr)
