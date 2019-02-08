5a7c6d8e27eb6e8d6667072dfd7efc280950f0fb
Blaine McMahon <42345023+BlaineMcMahon@users.noreply.github.com>
2019-02-08 17:17:37 -0500

    Add files via upload

--- Phase2/UDPserver.py
+++ Phase2/UDPserver.py
@@ -1,47 +1,104 @@
 import socket
 import select
 import time
-import sys
-import os
-import rdt
-
-UDP_IP = "127.0.0.1"	# Server IP
-IN_PORT = 12001	    # port
-DEFAULT_FILEPATH = '../spongebob.bmp'
-timeout = 3	# timeout
-
-if len(sys.argv) > 1:
-    response_filepath = sys.argv[1]
-else:
-    response_filepath = DEFAULT_FILEPATH
+from tkinter import *
+from PIL import Image, ImageTk
 
 # function to send an image
-def send_img(sock, client, filepath):
-    try:
-        f = open(filepath, 'rb')
-    except FileNotFoundError:
-        print(f'Response file {filepath} does not exist')
-        return
-    filename = os.path.basename(filepath)
-    print ("Sending filename of response")
-    sock.sendto(filename.encode('utf-8'), client)
-    print("Sending file response")
-    rdt.rdt_send(f, client, sock)
-    f.close()
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		# open a UDP socket
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# set the socket options to reuse address and port
-sock.bind((UDP_IP, IN_PORT))    # bind to address and port
-
-while True:
-    print('Server listening...')
-    msg, addr = sock.recvfrom(1024)
-    if msg:
-        fileName = msg.strip().decode('utf-8')
-        print ("Recieved file name:", fileName)
-
-    f = open(fileName, 'wb')
+# insert file name in sendto and open functions
+def sendImg(socket, client):
+	# send name of file
+    socket.sendto('file.png'.encode('utf-8'), client)
     
-    rdt.rdt_rcv(f, fileName, sock)
-    send_img(sock, addr, response_filepath)
+    print ("Sending...")
+
+	# open that image for reading binary
+    f = open("file.png", "rb")
+	
+	# read 1024 bytes
+    data = f.read(1024)
+
+	# while theres data keep sending
+    while(data):
+		# send 1024 bytes to client
+        if(socket.sendto(data, client)):
+			# read another 1024
+            data = f.read(1024)
+			# slight delay
+            time.sleep(0.02)
+
+
+def main():
+
+
+    UDP_IP = "127.0.0.1"    # Server IP
+    IN_PORT = 12001         # port
+    timeout = 3				# timeout
+
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		# open a UDP socket
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# set the socket options to reuse address and port
+    sock.bind((UDP_IP, IN_PORT))								# bind to address and port
+
+    # forever
+    while True:
+        data, addr = sock.recvfrom(1024)		# receive from client
+        if data:
+            data.decode
+            print ("File name:", data)			# print file received
+            file_name = data.strip()			# strip the header
+
+        f = open(file_name, 'wb')				# open that file for writing binary
+
+        while True:
+            ready = select.select([sock], [], [], timeout) # wait until sock is ready for I/O
+            if ready[0]:
+                data, addr = sock.recvfrom(1024)	# receive 1024 from client
+                f.write(data)						# write it to a file
+            else:
+                print ("%s Finish!" % file_name)	# finished writing 
+                f.close()							# close file
+                break
+
+       
+
+        sendImg(sock, addr)
+
+
+
+if __name__ == "__main__":
+
+
+    #key down function
+    def click():
+               
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        Label (window, text="Waiting for Client to be started....",bg ="black", fg ="white", font = "none 12 bold") .grid(row = 1, column = 0,)
+       
+        Label(window, image = photol2) .grid(row = 0, column = 0)
+
+        main()
+
+    #### main
+    window = Tk()
+    window.title("UDP Server")
+    window.configure(background = "black")
+    window.geometry("350x400") 
+    #### photo 
+    photol = PhotoImage(file = "C:\\Users\\Blaine\\Documents\\Network Design\\server.png")
+    photol2 = PhotoImage(file = "C:\\Users\\Blaine\\Documents\\Network Design\\l.png")
+    image_1 = Label(window, image = photol, bg = "black") .grid(row = 0, column = 0)
+
+    #### create a label
+    Label (window, text="Would you like to receive images from client? ",bg ="black", fg ="white", font = "none 12 bold") .grid(row = 1, column = 0, sticky = W)
+
+    #### add a submit button
+    Button(window, text = 'RUN', width = 6, command=click) .grid(row = 8, column = 0,)
+
+    #### run the main loop
+    window.mainloop()
+
+  
+
