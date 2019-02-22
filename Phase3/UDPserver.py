d8fd28c93ecae54f97c933a9ebcf26d6de0d8b9e
nyameen <nickyameen21@gmail.com>
2019-02-22 13:06:51 -0500

    Copied Phase2 files to Phase3

---
+++ Phase3/UDPserver.py
@@ -0,0 +1,53 @@
+import socket
+import select
+import time
+import sys
+import os
+import rdt
+
+DEFAULT_FILEPATH = 'spongebob.bmp'
+UDP_IP = "127.0.0.1"	# Server IP
+IN_PORT = 12001	    # port
+
+
+class UDPserver:
+    def __init__(self, filepath):
+        
+        self.response_filepath = filepath or DEFAULT_FILEPATH
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		# open a UDP socket
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# set the socket options to reuse address and port
+        self.sock.bind((UDP_IP, IN_PORT))    # bind to address and port
+
+    def send_img(self, client):
+        try:
+            f = open(self.response_filepath, 'rb') # open file for reading binary
+        except FileNotFoundError:
+            self.print(f'Response file {self.response_filepath} does not exist')
+            return
+        filename = os.path.basename(self.response_filepath)	# path to file
+        self.print("Sending filename of response")
+        self.sock.sendto(filename.encode('utf-8'), client) # send filename to client
+        self.print("Sending file response")
+        rdt.rdt_send(f, client, self.sock) # RDT send
+        f.close()
+
+    def listen(self):
+        while True:
+            self.print('Listening...')
+            msg, addr = self.sock.recvfrom(1024) # receive filename from client
+            
+            if msg:
+                fileName = msg.strip().decode('utf-8') # decode name 
+                self.print(f"Recieved file name: {fileName}")
+
+            fileName = f'server_recv_{fileName}' # append server_recv to filename
+            self.print(f'Writing filename {fileName}')
+            f = open(fileName, 'wb') # open for writing binary
+            
+            rdt.rdt_rcv(f, fileName, self.sock)	# RDT receive
+            self.print(f'Finished writing received file {fileName}')
+            self.send_img(addr) # respond by sending an image
+
+    def print(self, print_str):
+        print(f'Server: {print_str}') # print to the terminal
---
+++ Phase3/rdt.py
@@ -0,0 +1,90 @@
+import socket
+import select
+import time
+
+##       extract()
+##Parameters:
+##    sock     - the socket to send through
+##    bytesize - size to read
+##Return:
+##    the packet and data received
+##    0 if timeout
+def extract(sock, bytesize=1024):
+    timeout = 3
+    ready = select.select([sock], [], [], timeout)
+    if ready[0]:
+        data, addr = sock.recvfrom(1024)
+        return data
+    else:
+        return 0
+
+##       deliver_data()
+##Parameters:
+##    file  - file the write
+##    data  - the data received
+##Return:
+##    the packet and data received
+##    0 if timeout
+def deliver_data(file, data):
+    file.write(data)
+
+##       udt_send()
+##Parameters:
+##    packet - the packet to send
+##    server - the IP and Port number of server
+##    sock   - the socket to send through
+##Return:
+##    number of bytes sent
+def udt_send(packet, endpoint, sock):
+    return sock.sendto(packet, endpoint)
+
+##       make_pkt()
+##Parameters:
+##    file - the file to create a packet with
+##Return:
+##    the packet
+def make_pkt(file, bytesize=1024):
+    return file.read(bytesize)
+    
+##       rdt_send()
+##Parameters:
+##    file   - the file to be sent
+##    endpoint -
+##      If sending to server: (IP, PORT_NUMBER)
+##      If sending to client: client addr
+##    sock   - the socket to send through
+def rdt_send(file, endpoint, sock):
+    packet = make_pkt(file)
+    while packet:
+        if(udt_send(packet, endpoint, sock)):
+            packet = make_pkt(file)
+            time.sleep(0.005)
+
+##       rdt_rcv()
+##Parameters:
+##    file     - the file to be sent
+##    fileNeam - The name of the file
+##    sock     - the socket to send through            
+def rdt_rcv(file, fileName, sock):    
+    while True:
+        data = extract(sock)
+        if data:
+            deliver_data(file, data)
+        else:
+            file.close()
+            break
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        
