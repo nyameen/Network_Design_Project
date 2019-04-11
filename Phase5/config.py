3f0925530ea9dc0f099bd308b018cd012ab2a8db
Blaine McMahon <42345023+BlaineMcMahon@users.noreply.github.com>
2019-04-11 09:38:42 -0400

    Update config.py

--- Phase5/config.py
+++ Phase5/config.py
@@ -5,3 +5,16 @@ debug = 0   #1: print ack/data err statements, 0: no debug print statements
 timeout = 10 #ms
 window_size = 10
 max_buf_size = 65535
+
+#Default Server Settings
+MY_FILENAME = 'server_recv.jpg'
+DEFAULT_FILEPATH = 'spongebob.jpg'
+UDP_IP = "127.0.0.1"	# Server IP
+IN_PORT = 12001	    # port
+
+
+#Default Client Settings
+MY_FILENAME = 'server_recv.jpg'
+DEFAULT_FILEPATH = 'spongebob.jpg'
+UDP_IP = "127.0.0.1"	# Server IP
+IN_PORT = 12001	    # port
